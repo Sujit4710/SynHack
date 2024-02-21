010f5e234def1cbb20cef9c466203e1d67458568
Deo <Atharva.Deo@syngenta.com>
2024-02-21 23:46:59 +0530

    boiler code v2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Button from '@mui/material/Button';
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      
+      <Button variant="contained">Contained</Button>
+    </div>
+    
   )
 }
 
