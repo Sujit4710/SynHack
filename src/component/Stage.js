2f98155325d402a11aba23063b1a6192d7858dee
Deo <Atharva.Deo@syngenta.com>
2024-02-27 14:38:54 +0530

    demo 1.2

--- src/component/Stage.js
+++ src/component/Stage.js
@@ -4,28 +4,21 @@ import Points from "./Points";
 import StageCard from "./StageCard";
 import { Button } from "@mui/material";
 import Analysis from "./Analysis";
-<<<<<<< Updated upstream
 import que1 from "../Question/Stage1.json";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import Quizz from "../pages/Quizz";
 import QModal from "./QModal";
 
 const Stage = () => {
   const [detail, setDetail] = React.useState(false);
-  const [points,setPoints] = React.useState(0)
+  const [points, setPoints] = React.useState(0);
   const navigate = useNavigate();
-  console.log("he",que1);
- 
-=======
-
-const Stage = () => {
-  const [detail, setDetail] = React.useState(false);
->>>>>>> Stashed changes
+  console.log("he", que1);
   return (
     <>
       {detail ? (
         <div>
-             <div
+          <div
             style={{
               display: "flex",
               justifyContent: "flex-start",
@@ -33,27 +26,22 @@ const Stage = () => {
           >
             {" "}
             <button
-               onClick={() => setDetail(false)}
+              onClick={() => setDetail(false)}
               className={styles["btn-left"]}
             >
               Back
             </button>
           </div>
 
-<div>
-  <Analysis/>
-</div>
-          
+          <div>
+            <Analysis />
+          </div>
         </div>
       ) : (
         <div className={styles["Stage"]}>
           <div>
             {" "}
-<<<<<<< Updated upstream
             <Points points={points} />
-=======
-            <Points />
->>>>>>> Stashed changes
           </div>
 
           <div
@@ -64,50 +52,47 @@ const Stage = () => {
               marginTop: "50px",
             }}
           >
-<<<<<<< Updated upstream
-      
-            <QModal content={ <StageCard
-              heading="Stage 1"
-              text="Land preparation & seed Selection."
-              description=" Cultivate Knowledge, Plant Seeds of Sustainability, and Watch Your Eco-IQ Blossom!"
-              placement="right"
-            />} que = "1" points={points} setPoints={setPoints}/>
-           
-            <QModal content={ <StageCard
-              heading="Stage 2"
-              text="Planting & Monitoring"
-              description="GreenHarvest: Planting & Monitoring – where you nurture your green expertise, sow seeds of environmental wisdom, and watch your sustainable know-how sprout to life!"
-              placement="right"
-            />} que = "2" points={points} setPoints={setPoints}/>
-            
-            <QModal content={<StageCard
-              heading="Stage 3"
-              text="Crop protection & harvesting"
-              description="GreenHarvest: Crop Protection & Harvesting – guard your green gains, reap a bounty of eco-knowledge, and celebrate the sustainable harvest of your environmental expertise!"
-              placement="left"
+            <QModal
+              content={
+                <StageCard
+                  heading="Stage 1"
+                  text="Land preparation & seed Selection."
+                  description=" Cultivate Knowledge, Plant Seeds of Sustainability, and Watch Your Eco-IQ Blossom!"
+                  placement="right"
+                />
+              }
+              que="1"
+              points={points}
+              setPoints={setPoints}
             />
-          } que = "3" points={points} setPoints={setPoints} />
-          
-=======
-            <StageCard
-              heading="Stage 1"
-              text="Land preparation & seed Selection."
-              description="asff"
-              placement="right"
+
+            <QModal
+              content={
+                <StageCard
+                  heading="Stage 2"
+                  text="Planting & Monitoring"
+                  description="GreenHarvest: Planting & Monitoring – where you nurture your green expertise, sow seeds of environmental wisdom, and watch your sustainable know-how sprout to life!"
+                  placement="right"
+                />
+              }
+              que="2"
+              points={points}
+              setPoints={setPoints}
             />
-            <StageCard
-              heading="Stage 2"
-              text="Planting & Monitoring"
-              description="asff"
-              placement="right"
+
+            <QModal
+              content={
+                <StageCard
+                  heading="Stage 3"
+                  text="Crop protection & harvesting"
+                  description="GreenHarvest: Crop Protection & Harvesting – guard your green gains, reap a bounty of eco-knowledge, and celebrate the sustainable harvest of your environmental expertise!"
+                  placement="left"
+                />
+              }
+              que="3"
+              points={points}
+              setPoints={setPoints}
             />
-            <StageCard
-              heading="Stage 3"
-              text="Crop protection & harvesting"
-              description="asff"
-              placement="left"
-            />
->>>>>>> Stashed changes
           </div>
 
           <div
@@ -119,27 +104,16 @@ const Stage = () => {
             }}
           >
             {" "}
-<<<<<<< Updated upstream
             {/* <button
-=======
-            <button
->>>>>>> Stashed changes
                onClick={() => setDetail(true)}
               className={styles["btn-right"]}
             >
               Detailed Analysis
-<<<<<<< Updated upstream
             </button> */}
-=======
-            </button>
->>>>>>> Stashed changes
           </div>
         </div>
       )}
     </>
   );
 };
-
 export default Stage;
-
-
